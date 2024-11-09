ba0a5a3fde82ce596caf2c0fa590fe7ac9785f3b
Satya Prakash Mohanty <mebubu03@gmail.com>
2024-11-09 23:54:14 +0530

    Adding final images

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8000)
